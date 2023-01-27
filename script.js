8126a366debffc5d215dd2b8854cc65e12bc7eca
DominikKnapczyk <dominik.kna.01@gmail.com>
2023-01-27 16:48:07 +0100

    JSnack 3

--- script.js
+++ script.js
@@ -12,3 +12,41 @@ function snack1Function() {
     output1.innerHTML = input1 ;
   }
 }
+
+
+
+
+
+// ESERCIZIO JSnack2 -------------------------------------------------------------------------------------------------------
+
+let snack2_array = [45, 3, 2, 88, 131, 412, 632, 43, 9, 224, 86, 45];
+var output2red = document.getElementById("div_red");
+var output2green = document.getElementById("div_green");
+
+for (let i = 0; i < snack2_array.length; i++) {
+
+  if (snack2_array[i] % 2 == 0) {
+    output2green.innerHTML +=  snack2_array[i] + "/" ;
+  } else {
+    output2red.innerHTML += snack2_array[i] + "/";
+  }
+}
+
+
+
+
+// ESERCIZIO JSnack3 -------------------------------------------------------------------------------------------------------
+
+
+let snack3_array = [66, 78, 32, 1, 4, 56, 373, 24, 98, 124, 153, 33, 79];
+var output3= document.getElementById("output3");
+let somma = 0;
+
+for (let i = 0; i < snack2_array.length; i++) {
+  
+  if (i % 2 !== 0) {
+    somma += snack3_array[i];
+  }
+}
+
+output3.innerHTML += somma;
